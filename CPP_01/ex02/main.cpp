14ced6a8dfff25606c2a0f438135a055f69addd3
marwan0620@gmail.com <marwan0620@gmail.com>
2023-02-20 10:18:14 +0100

    error of string info fix

--- CPP_01/ex02/main.cpp
+++ CPP_01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mayoub <mayoub@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 15:55:50 by mayoub            #+#    #+#             */
-/*   Updated: 2023/02/18 16:02:18 by mayoub           ###   ########.fr       */
+/*   Updated: 2023/02/20 10:16:43 by mayoub           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,13 @@ int	main(void)
 	std::string	*stringPTR = &brain;
 	std::string	&stringREF = brain;
 
-	std::cout << "address of [brain], the VARIABLE =	" << &brain << std::endl;
-	std::cout << "address of [brain], the POINTER =	" << stringPTR << std::endl;
-	std::cout << "address of [brain], the REFERENCE =	" << &stringREF << std::endl;
+	std::cout << "[address] of [brain]:     the VARIABLE =	" << &brain << std::endl;
+	std::cout << "[address] of [stringPTR]: the POINTER =		" << stringPTR << std::endl;
+	std::cout << "[address] of [stringREF]: the REFERENCE =	" << &stringREF << std::endl << std::endl;
 
-
-	std::cout << "value of [brain], the VARIABLE =	" << brain << std::endl;
-	std::cout << "address of [brain], the POINTER =	" << *stringPTR << std::endl;
-	std::cout << "address of [brain], the REFERENCE =	" << stringREF << std::endl;
+	std::cout << "[value] of [brain]:	the VARIABLE =		" << brain << std::endl;
+	std::cout << "[value] of [stringPTR]:	the POINTER =		" << *stringPTR << std::endl;
+	std::cout << "[value] of [stringREF]:	the REFERENCE =		" << stringREF << std::endl;
 
 	return (0);
 }
