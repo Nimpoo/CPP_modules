10a008e8bb1dde0b391907205b769b33ec02adb6
Marwan Ayoub <marwan0620@gmail.com>
2023-04-18 17:58:30 +0200

    CPP_06/ex00 🙃

--- CPP_06/ex00/main.cpp
+++ CPP_06/ex00/main.cpp
@@ -6,19 +6,28 @@
 /*   By: sihemayoub <sihemayoub@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 12:55:43 by sihemayoub        #+#    #+#             */
-/*   Updated: 2023/04/17 18:54:26 by sihemayoub       ###   ########.fr       */
+/*   Updated: 2023/04/18 17:57:11 by sihemayoub       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <iomanip>
 #include <cstdlib>
+#include <limits>
 
 bool	isNumber( const std::string &s )
 {
+	if (s[1] == '+' || s[1] == '-')
+		return false;
+
 	for (int i = 0; s[i]; i++) {
 		if ((s[i] == '-' && i == 0) || (s[i] == '+' && i == 0) || (s[i] == '.'))
 			i++;
+
+		if (s.substr(i, s.size()) == "inf" || s.substr(i, s.size()) == "inff"
+		 || s.substr(i, s.size()) == "nan" || s.substr(i, s.size()) == "nanf")
+			return true;
+
 		if (std::isdigit(s[i]) == 0)
 			return false;
 	}
@@ -28,6 +37,15 @@ bool	isNumber( const std::string &s )
 void	parseDouble( std::string real ) {
 
 	double const	cast = static_cast<double>(real.c_str()[0]);
+
+	if ((!real.compare("inf") || !real.compare("inff") || !real.find("+inf") || !real.find("-inf")) && isNumber(real))
+	{
+		if (real[0] == '-')
+			std::cout << "float: -" << std::numeric_limits<double>::infinity() << std::endl;
+		else
+			std::cout << "float: +" << std::numeric_limits<double>::infinity() << std::endl;
+		return ;
+	}
 
 	if (real.size() != 1 && atof(real.c_str()) < 0 && isNumber(real))
 		std::cout << std::fixed << "double: " << atof(real.c_str()) << std::endl;
@@ -43,6 +61,15 @@ void	parseFloat( std::string floater ) {
 
 	float const	cast = static_cast<float>(floater.c_str()[0]);
 
+	if ((!floater.compare("inf") || !floater.compare("inff") || !floater.find("+inf") || !floater.find("-inf")) && isNumber(floater))
+	{
+		if (floater[0] == '-')
+			std::cout << "float: -" << std::numeric_limits<float>::infinity() << "f" << std::endl;
+		else
+			std::cout << "float: +" << std::numeric_limits<float>::infinity() << "f" << std::endl;
+		return ;
+	}
+
 	if (floater.size() != 1 && atof(floater.c_str()) < 0 && isNumber(floater))
 		std::cout << std::fixed << "float: " << atof(floater.c_str()) << "f" << std::endl;
 	else if (floater.size() != 1 && !isNumber(floater))
@@ -57,6 +84,12 @@ void	parseInt( std::string integer ) {
 
 	int const	cast = static_cast<int>(integer.c_str()[0]);
 
+	if ((!integer.compare("inf") || !integer.compare("inff") || !integer.find("nan") || !integer.find("nanf") || !integer.find("+inf") || !integer.find("-inf")) && !isNumber(integer))
+	{
+		std::cout << "int: impossible" << std::endl;
+		return ;
+	}
+
 	if (integer.size() != 1 && atoi(integer.c_str()) < 0 && isNumber(integer))
 		std::cout << "int: " << atoi(integer.c_str()) << std::endl;
 	else if (integer.size() != 1 && !isNumber(integer))
@@ -70,6 +103,12 @@ void	parseInt( std::string integer ) {
 void	parseChar( std::string character ) {
 
 	const char	*cast = character.c_str();
+
+	if ((!character.compare("inf") || !character.compare("inff") || !character.find("nan") || !character.find("nanf") || !character.find("+inf") || !character.find("-inf")) && !isNumber(character))
+	{
+		std::cout << "char: impossible" << std::endl;
+		return ;
+	}
 
 	if (((atoi(cast) > 126 || atoi(cast) < 0)) || (character.size() != 1 && !isNumber(character)))
 		std::cout << "char: impossible" << std::endl;
@@ -127,11 +166,13 @@ int	main(int ac, char **av)
 		else parseInt( string_av ); 
 
 		if (flt && nbr && !point && !chr) parseFloat( string_av.substr(0, string_av.size() - 1) );
-		else if ((!flt && nbr && point && !chr) || (flt && nbr && point && !chr)) parseFloat( string_av.substr(0, string_av.size()) );
+		else if (!flt && nbr && point && !chr) parseFloat( string_av.substr(0, string_av.size()) + "0" );
+		else if (flt && nbr && point && !chr) parseFloat( string_av.substr(0, string_av.size() - 1) + "0" );
 		else parseFloat( string_av );
 
 		if (flt && nbr && !point && !chr) parseDouble( string_av.substr(0, string_av.size() - 1) );
-		else if ((!flt && nbr && point && !chr) || (flt && nbr && point && !chr)) parseDouble( string_av.substr(0, string_av.size()) );
+		else if (!flt && nbr && point && !chr) parseDouble( string_av.substr(0, string_av.size()) + "0" );
+		else if (flt && nbr && point && !chr) parseDouble( string_av.substr(0, string_av.size() - 1) + "0" );
 		else parseDouble( string_av );
 	}
 	return (0);
