31996082b939568512551ddad10ef33734037572
Marwan Ayoub <marwan0620@gmail.com>
2023-04-19 20:07:17 +0200

    CPP_07/ex00 init

---
+++ CPP_07/ex00/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sihemayoub <sihemayoub@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/19 19:50:07 by sihemayoub        #+#    #+#             */
+/*   Updated: 2023/04/19 20:06:47 by sihemayoub       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+
+int	main(void)
+{
+	// int a = 2;
+	// int b = 3;
+
+	// ::swap( a, b );
+	// std::cout << "a = " << a << ", b = " << b << std::endl;
+	// std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	// std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+	// std::string c = "chaine1";
+	// std::string d = "chaine2";
+
+	// ::swap(c, d);
+	// std::cout << "c = " << c << ", d = " << d << std::endl;
+	// std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	// std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+
+	return (0);
+}
