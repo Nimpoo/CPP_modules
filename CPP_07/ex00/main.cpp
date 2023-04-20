664bff2c314c157baea76b1727baf130022b2a8a
Marwan Ayoub <marwan0620@gmail.com>
2023-04-20 11:52:31 +0200

    CPP_07/ex00 finished

--- CPP_07/ex00/main.cpp
+++ CPP_07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sihemayoub <sihemayoub@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 19:50:07 by sihemayoub        #+#    #+#             */
-/*   Updated: 2023/04/19 20:06:47 by sihemayoub       ###   ########.fr       */
+/*   Updated: 2023/04/20 11:51:27 by sihemayoub       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,21 @@
 
 int	main(void)
 {
-	// int a = 2;
-	// int b = 3;
+	int a = 2;
+	int b = 3;
 
-	// ::swap( a, b );
-	// std::cout << "a = " << a << ", b = " << b << std::endl;
-	// std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-	// std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
 
-	// std::string c = "chaine1";
-	// std::string d = "chaine2";
+	std::string c = "chaine1";
+	std::string d = "chaine2";
 
-	// ::swap(c, d);
-	// std::cout << "c = " << c << ", d = " << d << std::endl;
-	// std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-	// std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 
 	return (0);
 }
--- CPP_07/ex00/whatever.hpp
+++ CPP_07/ex00/whatever.hpp
@@ -6,7 +6,7 @@
 /*   By: sihemayoub <sihemayoub@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 19:50:55 by sihemayoub        #+#    #+#             */
-/*   Updated: 2023/04/19 20:06:53 by sihemayoub       ###   ########.fr       */
+/*   Updated: 2023/04/20 11:50:56 by sihemayoub       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,20 @@
 
 # include <iostream>
 
+template< typename T >
+void	swap( T &x, T &y ) {
 
+	T swap;
+
+	swap = x;
+	x = y;
+	y = swap;
+}
+
+template< typename T >
+T const	&min( T const &x, T const &y ) { return ( x < y ? x : y ); }
+
+template< typename T >
+T const	&max( T const &x, T const &y ) { return ( x > y ? x : y ); }
 
 #endif
